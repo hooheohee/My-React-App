0d1812cf8861b6446b8da88d96387219f252f994
Yue Zheng <zheng.yue1@husky.neu.edu>
2019-06-04 22:21:29 -0400

    completed basic/rich text functions

--- src/SyncingEditor.tsx
+++ src/SyncingEditor.tsx
@@ -42,40 +42,123 @@ export const SyncingEditor: React.FC<Props> = ({ groupId }) => {
   }, [groupId]);
 
   return (
-    <Editor
-      ref={editor}
-      style={{
-        backgroundColor: "#fafafa",
-        maxWidth: 800,
-        minHeight: 150
-      }}
-      value={value}
-      onChange={opts => {
-        setValue(opts.value);
+    <>
+      <button
+        style={{
+          padding: "0.5em",
+          fontSize: "15px",
+          margin: "1em 1em 1em 2em"
+        }}
+        onMouseDown={e => {
+          e.preventDefault();
+          // bold
+          editor.current!.toggleMark("bold");
+        }}
+      >
+        bold
+      </button>
+      <button
+        style={{
+          padding: "0.5em",
+          fontSize: "15px",
+          margin: "1em"
+        }}
+        onMouseDown={e => {
+          e.preventDefault();
+          // Italic
+          editor.current!.toggleMark("italic");
+        }}
+      >
+        Italic
+      </button>
+      <button
+        style={{
+          padding: "0.5em",
+          fontSize: "15px",
+          margin: "1em"
+        }}
+        onMouseDown={e => {
+          e.preventDefault();
+          // code
+          editor.current!.toggleMark("code");
+        }}
+      >
+        code
+      </button>
+      <button
+        style={{
+          padding: "0.5em",
+          fontSize: "15px",
+          margin: "1em"
+        }}
+        onMouseDown={e => {
+          e.preventDefault();
+          // underlined
+          editor.current!.toggleMark("underlined");
+        }}
+      >
+        underlined
+      </button>
+      <Editor
+        ref={editor}
+        style={{
+          padding: "1em",
+          margin: "0 1em 1em 1em",
+          backgroundColor: "#f0f0f0",
+          maxWidth: 800,
+          minHeight: 250,
+          border: "solid gray 1px"
+        }}
+        value={value}
+        renderMark={(props, _editor, next) => {
+          if (props.mark.type === "bold") {
+            return <strong>{props.children}</strong>;
+          } else if (props.mark.type === "italic") {
+            return <em>{props.children}</em>;
+          } else if (props.mark.type === "code") {
+            return (
+              <code
+                style={{
+                  backgroundColor: "white",
+                  padding: "0.5em",
+                  border: "solid gray 1px"
+                }}
+              >
+                {props.children}
+              </code>
+            );
+          } else if (props.mark.type === "underlined") {
+            return <u>{props.children}</u>;
+          }
+          return next();
+        }}
+        onChange={opts => {
+          setValue(opts.value);
 
-        const ops = opts.operations
-          .filter(o => {
-            if (o) {
-              return (
-                o.type !== "set_selection" &&
-                o.type !== "set_value" &&
-                (!o.data || !o.data.has("source"))
-              );
-            }
-            return false;
-          })
-          .toJS()
-          .map((o: any) => ({ ...o, data: { source: "one" } }));
+          const ops = opts.operations
+            .filter(o => {
+              if (o) {
+                return (
+                  o.type !== "set_selection" &&
+                  o.type !== "set_value" &&
+                  (!o.data || !o.data.has("source"))
+                );
+              }
+              return false;
+            })
+            .toJS()
+            .map((o: any) => ({ ...o, data: { source: "one" } }));
 
-        if (ops.length && !remote.current) {
-          socket.emit("new-operations", {
-            editorId: id.current,
-            ops,
-            value: opts.value.toJSON(),
-            groupId
-          });
-        }
-      }}
-    />
+          if (ops.length && !remote.current) {
+            socket.emit("new-operations", {
+              editorId: id.current,
+              ops,
+              value: opts.value.toJSON(),
+              groupId
+            });
+          }
+        }}
+      />
+    </>
   );
 };
